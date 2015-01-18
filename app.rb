b4ebe0c2a4572343bb5ca41f9443bde2aaa8b8ac
Travis Briggs <audiodude@gmail.com>
2015-01-18 22:42:53 +0000

    Working version without default value

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'sinatra/content_for'
 require 'sqlite3'
 
-PATTERN = /\(:([^)]+)\)/
+PATTERN = /\(:([^)]*)\)/
+         #/\(:([^|]+)\|?([^)]*)\)/
+         #/\(:([^)]*)\)/ ----> original
 
 def get_madlib_by_id(id)
   db = SQLite3::Database.new 'mad_libs.sqlite'
@@ -14,7 +16,7 @@ def get_madlib_by_id(id)
   
   # TODO: Handle the error case where the supposed "mad lib" doesn't match PATTERN at all.
   content.scan(PATTERN) do | match |
-    pattern_array.push($1)
+    pattern_array.push($1)  # $2 is the default value.
   end
   mad_lib << pattern_array  # Now mad_lib[4] is the pattern array
 end
@@ -35,9 +37,7 @@ post '/do/:id' do
   @mad_lib = get_madlib_by_id(params[:id])
   
   # TODO: Handle the error case where there is no mad lib with that ID
-  
-  @box = 'error-input-box'
-  
+    
   @input_array = params[:input_array]
   empty_input = @input_array.detect do |input|
     input.empty?
@@ -73,31 +73,18 @@ get '/admin/new' do
 end
 
 post '/admin/new' do
-  db = SQLite3::Database.new 'mad_libs.sqlite'
-  db.execute('INSERT INTO mad_libs (title, description, content) VALUES(?, ?, ?)',
-              [params[:title], params[:description], params[:content]])
-  
-=begin
-  @box = 'error-input-box'
-  
   @title = params[:title]
-  empty_input = @title.detect do |input|
-    input.empty?
-  end
   @description = params[:description]
-  empty_input = @description.detect do |input|
-    input.empty?
-  end
   @content = params[:content]
-  empty_input = @content.detect do |input|
-    input.empty?
-  end
   
-  if empty_input
+  if @title.empty? || @description.empty? || @content.empty?
     @error = "Please fill out all fields."
     return erb :new
   else
+    db = SQLite3::Database.new 'mad_libs.sqlite'
+    db.execute('INSERT INTO mad_libs (title, description, content) VALUES(?, ?, ?)',
+              [params[:title], params[:description], params[:content]])
+    redirect '/'
   end
-=end
 end
 
