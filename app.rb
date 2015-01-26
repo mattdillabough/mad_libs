0832bbcf489db2177b0c37e4130e3744811d191d
Travis Briggs <audiodude@gmail.com>
2015-01-26 05:12:38 +0000

    Working default value functionality

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'sinatra'
 require 'sinatra/content_for'
 require 'sqlite3'
 
-PATTERN = /\(:([^)]*)\)/
-         #/\(:([^|]+)\|?([^)]*)\)/
+#PATTERN = /\(:([^)]*)\)/
+PATTERN = /\(:([^|)]+)\|?([^)]*)\)/
+         #/\(:([^|]+)\|?([^)]*)\)/ ----> updated version
          #/\(:([^)]*)\)/ ----> original
 
 def get_madlib_by_id(id)
@@ -13,10 +14,10 @@ def get_madlib_by_id(id)
   mad_lib = mad_libs[0]
   content = mad_lib[3]
   pattern_array = []
-  
+   
   # TODO: Handle the error case where the supposed "mad lib" doesn't match PATTERN at all.
   content.scan(PATTERN) do | match |
-    pattern_array.push($1)  # $2 is the default value.
+    pattern_array << [$1, $2]  # $2 is the default value.
   end
   mad_lib << pattern_array  # Now mad_lib[4] is the pattern array
 end
@@ -37,10 +38,13 @@ post '/do/:id' do
   @mad_lib = get_madlib_by_id(params[:id])
   
   # TODO: Handle the error case where there is no mad lib with that ID
-    
+  
   @input_array = params[:input_array]
-  empty_input = @input_array.detect do |input|
-    input.empty?
+  @input_with_defaults = @input_array.zip(@mad_lib[4])
+  empty_input = @input_with_defaults.detect do |input_with_default|
+    # input_with_default[0] is the input, ...[1][1] is the default
+    # Detect if they are both empty.
+    input_with_default[0].empty? && input_with_default[1][1].empty?
   end
   
   if empty_input
@@ -50,9 +54,14 @@ post '/do/:id' do
     @mad_lib[3] =~ PATTERN
     @output = ''
     
-    @input_array.each_with_index do | match, i |
+    @input_with_defaults.each_with_index do | input_with_default, i |
       # TODO: Handle the error case where the supposed "mad lib" doesn't match PATTERN at all.
-      @output += $` + match
+      
+      if input_with_default[0].empty?
+        @output += $` + input_with_default[1][1]
+      else
+        @output += $` + input_with_default[0]
+      end
       if i == @input_array.size - 1
         @output += $'
       else
